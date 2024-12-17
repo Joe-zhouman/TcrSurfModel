fix: verbose error in backprop; add: cam for cnn visual

visual/torch/cnn/backprop.py
@@ -5,28 +5,36 @@
 import torch
 import torch.nn as nn
 
-from typing import List
+from typing import List, Tuple
 from abc import ABCMeta
 
 
 class BackpropBase(metaclass=ABCMeta):
-
+    """
+    参数:
+        - model (nn.Module): 需要进行梯度计算的PyTorch模型。
+        - device (str): 模型和输入将被发送到的设备。默认为CUDA（如果可用），否则为CPU。
+        - verbose (bool): 是否打印详细信息。默认为False。
+    """
     def __init__(
         self,
         model: nn.Module,
         device: str = "cuda" if torch.cuda.is_available() else "cpu",
         verbose: bool = False,
     ):
+        """
+        初始化模型和设备，并设置模型为评估模式。
+        """
         self.device = device
-        # 将模型移动到指定设备
         self.model = model.to(self.device)
         # 初始化梯度属性为None，稍后将通过前向传播计算梯度
         self.gradients = None
         # 将模型设置为评估模式，以关闭Dropout等仅在训练时需要的特性
         self.model.eval()
+        # 初始化verbose参数，用于控制是否打印详细信息
+        self.verbose = verbose
         # 在目标层上挂载钩子，以提取所需的中间层输出
         self.hook_first_conv_layer()
-        self.verbose = verbose
 
     def hook_first_conv_layer(self):
         """
@@ -44,7 +52,7 @@ class BackpropBase(metaclass=ABCMeta):
                 # 只需第一个卷积层
                 break
 
-    def generate_gradients(self, batch, loss_func):
+    def generate_gradients(self, batch, loss_func: nn.Module):
         """
         计算模型参数相对于损失的梯度。
 
@@ -55,22 +63,16 @@ class BackpropBase(metaclass=ABCMeta):
         返回:
         - gradients_as_arr: 包含模型参数梯度的数组。
         """
-
-        # 准备批数据，启用每个项的梯度计算
-        batch = [item.requires_grad_().to(self.device) for item in batch]
         #! 重要提示：没有 requires_grad_() 就不会计算梯度
-        # 特别确保批数据中的第一项启用了梯度计算，以防在上面的循环中遗漏
-        batch[0] = batch[0].requires_grad_()
-        # 从批数据中提取目标标签
+        batch = [item.to(self.device).requires_grad_() for item in batch]
         targets = batch[-1]
         # 前向传播：计算模型的输出
         outputs = self.model(*batch[0:-1])
         # 将梯度重置为零，为下一次反向传播做准备
         self.model.zero_grad()
-        # 计算当前损失
         current_loss = loss_func(outputs, targets)
-        # 打印当前损失，以便在训练过程中进行监控
-        print(current_loss)
+        if self.verbose:
+            print(current_loss)
         # 反向传播：计算梯度
         current_loss.backward()
         gradients_as_arr = self.gradients.data.cpu().numpy()[0]
@@ -80,6 +82,10 @@ class BackpropBase(metaclass=ABCMeta):
 class VanillaBackprop(BackpropBase):
     """
     普通反向传播
+    参数:
+        - model (nn.Module): 需要进行梯度计算的PyTorch模型。
+        - device (str): 模型和输入将被发送到的设备。默认为CUDA（如果可用），否则为CPU。
+        - verbose (bool): 是否打印详细信息。默认为False。
     """
 
     pass
@@ -88,6 +94,11 @@ class VanillaBackprop(BackpropBase):
 class GuidedBackprop(BackpropBase):
     """
     导向反向传播
+    参数:
+        - model (nn.Module): 需要进行梯度计算的PyTorch模型。
+        - relu_modules: List[nn.Module]，包含需要被替换为支持梯度计算的ReLU模块的列表. #! 对于表面模型, 为[surf_model.pretrained_net]
+        - device (str): 模型和输入将被发送到的设备。默认为CUDA（如果可用），否则为CPU。
+        - verbose (bool): 是否打印详细信息。默认为False。
     """
 
     def __init__(
@@ -103,7 +114,6 @@ class GuidedBackprop(BackpropBase):
 
         参数:
         - model: nn.Module，待解释的神经网络模型。
-        - relu_modules: List[nn.Module]，包含需要被替换为支持梯度计算的ReLU模块的列表。
         - device: str，模型和数据所使用的计算设备，默认为CUDA（如果可用），否则为CPU。
         """
         # 初始化前向传播ReLU输出的列表，用于存储中间激活值
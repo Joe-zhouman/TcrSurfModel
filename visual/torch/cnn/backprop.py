0f87a22c0ab6860fc02929547f78444e21e1e2cc
Joe-zhouman <man.man.man.man.a@gmail.com>
2024-12-17 19:52:37 +0800

    fix: verbose error in backprop; add: cam for cnn visual

--- visual/torch/cnn/backprop.py
+++ visual/torch/cnn/backprop.py
@@ -5,28 +5,36 @@
 import torch
 import torch.nn as nn
 
-from typing import List
+from typing import List, Tuple
 from abc import ABCMeta
 
 
 class BackpropBase(metaclass=ABCMeta):
-
+    """
+    参数:
+        - model (nn.Module): 需要进行梯度计算的PyTorch模型。
+        - device (str): 模型和输入将被发送到的设备。默认为CUDA（如果可用），否则为CPU。
+        - verbose (bool): 是否打印详细信息。默认为False。
+    """
     def __init__(
         self,
         model: nn.Module,
         device: str = "cuda" if torch.cuda.is_available() else "cpu",
         verbose: bool = False,
     ):
+        """
+        初始化模型和设备，并设置模型为评估模式。
+        """
         self.device = device
-        # 将模型移动到指定设备
         self.model = model.to(self.device)
         # 初始化梯度属性为None，稍后将通过前向传播计算梯度
         self.gradients = None
         # 将模型设置为评估模式，以关闭Dropout等仅在训练时需要的特性
         self.model.eval()
+        # 初始化verbose参数，用于控制是否打印详细信息
+        self.verbose = verbose
         # 在目标层上挂载钩子，以提取所需的中间层输出
         self.hook_first_conv_layer()
-        self.verbose = verbose
 
     def hook_first_conv_layer(self):
         """
@@ -44,7 +52,7 @@ class BackpropBase(metaclass=ABCMeta):
                 # 只需第一个卷积层
                 break
 
-    def generate_gradients(self, batch, loss_func):
+    def generate_gradients(self, batch, loss_func: nn.Module):
         """
         计算模型参数相对于损失的梯度。
 
@@ -55,22 +63,16 @@ class BackpropBase(metaclass=ABCMeta):
         返回:
         - gradients_as_arr: 包含模型参数梯度的数组。
         """
-
-        # 准备批数据，启用每个项的梯度计算
-        batch = [item.requires_grad_().to(self.device) for item in batch]
         #! 重要提示：没有 requires_grad_() 就不会计算梯度
-        # 特别确保批数据中的第一项启用了梯度计算，以防在上面的循环中遗漏
-        batch[0] = batch[0].requires_grad_()
-        # 从批数据中提取目标标签
+        batch = [item.to(self.device).requires_grad_() for item in batch]
         targets = batch[-1]
         # 前向传播：计算模型的输出
         outputs = self.model(*batch[0:-1])
         # 将梯度重置为零，为下一次反向传播做准备
         self.model.zero_grad()
-        # 计算当前损失
         current_loss = loss_func(outputs, targets)
-        # 打印当前损失，以便在训练过程中进行监控
-        print(current_loss)
+        if self.verbose:
+            print(current_loss)
         # 反向传播：计算梯度
         current_loss.backward()
         gradients_as_arr = self.gradients.data.cpu().numpy()[0]
@@ -80,6 +82,10 @@ class BackpropBase(metaclass=ABCMeta):
 class VanillaBackprop(BackpropBase):
     """
     普通反向传播
+    参数:
+        - model (nn.Module): 需要进行梯度计算的PyTorch模型。
+        - device (str): 模型和输入将被发送到的设备。默认为CUDA（如果可用），否则为CPU。
+        - verbose (bool): 是否打印详细信息。默认为False。
     """
 
     pass
@@ -88,6 +94,11 @@ class VanillaBackprop(BackpropBase):
 class GuidedBackprop(BackpropBase):
     """
     导向反向传播
+    参数:
+        - model (nn.Module): 需要进行梯度计算的PyTorch模型。
+        - relu_modules: List[nn.Module]，包含需要被替换为支持梯度计算的ReLU模块的列表. #! 对于表面模型, 为[surf_model.pretrained_net]
+        - device (str): 模型和输入将被发送到的设备。默认为CUDA（如果可用），否则为CPU。
+        - verbose (bool): 是否打印详细信息。默认为False。
     """
 
     def __init__(
@@ -103,7 +114,6 @@ class GuidedBackprop(BackpropBase):
 
         参数:
         - model: nn.Module，待解释的神经网络模型。
-        - relu_modules: List[nn.Module]，包含需要被替换为支持梯度计算的ReLU模块的列表。
         - device: str，模型和数据所使用的计算设备，默认为CUDA（如果可用），否则为CPU。
         """
         # 初始化前向传播ReLU输出的列表，用于存储中间激活值
---
+++ visual/torch/cnn/cam.py
@@ -0,0 +1,235 @@
+import torch
+
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+import torchvision.models as models
+
+from collections import OrderedDict
+from typing import List, Callable, Optional
+
+from util.model.surf.modified_cnn_model import ModifiedPretrainedNet
+from PIL import Image
+from abc import ABCMeta, abstractmethod
+
+
+class CamBase(metaclass=ABCMeta):
+    """
+    Extracts cam features from the model
+    表面模型架构如下:
+
+    ![surf_model架构示意图](https://s2.loli.net/2024/12/17/2xAlQN3Hb7gyjI1.png)
+
+    对应输入为:
+    - model:surf_model
+    - feature_layers:[surf_model.adaptor(如果有adaptor模块),
+    surf_model.pretrained_net.pretrained_net.features(最后的名字可能不一样)]
+    #! 注意, 要按顺序传入.
+    - pretrained_net_classification_layer:surf_model.pretrained_net.pretrained_net.classifier(最后的名字可能不一样)
+    feature和classifier的具体名字可以通过以下代码查看:
+
+    >>> print([name for name, _ in surf_model.pretrained_net.pretrained_net.named_children()])
+    #for densenet,output: ['features', 'classifier']
+
+    - target_layer: "denseblock1"(这里以densenet121为例) 即surf_model.pretrained_net.pretrained_net.features下面一个子层的名字.
+    可以使用如下代码参考这些层的名字:
+
+    >>> print([name for name, _ in surf_model.pretrained_net.pretrained_net.features.named_children()])
+    """
+
+    def __init__(
+        self,
+        model: nn.Module,
+        device: str = "cuda" if torch.cuda.is_available() else "cpu",
+        verbose: bool = False,
+    ):
+        self.model = model
+        self.device = device
+        self.verbose = verbose
+        self.model.to(self.device)
+        self.model.eval()
+
+        self._gradients = None
+        self.__pretrained_net = self.model.pretrained_net.pretrained_net
+        self.__pnet_feature_to_classifier_opt = self.__forward()
+        self.__feature_layer_dict = OrderedDict()
+
+        self.__get_feature_dict()
+        self.__print_target_layer()
+
+        self._target_layer = None
+        self._cam = None
+        self._weights = None
+        self._conv_outputs = None
+        self._batch = None
+
+    def __set_target_layer(self, target_layer: Optional[str]):
+        self._target_layer = target_layer
+        if self._target_layer is None:
+            self._target_layer = list(self.__feature_layer_dict.keys())[-1]
+            if self.verbose:
+                print(
+                    f"No target layer specified, using the last featuring layer [{self._target_layer}]"
+                )
+        self._target_layer = target_layer
+
+    def __print_target_layer(self):
+        if self.verbose:
+            print(f"Following target layer is available:")
+            print(f"{[name for name in self.__feature_layer_dict]}")
+
+    def __save_gradient(self, grad):
+        self._gradients = grad
+
+    def __forward_pass_on_convolutions(self, x):
+        """
+        Does a forward pass on convolutions, hooks the function at given layer
+        """
+        conv_output = None
+        for name in self.__feature_layer_dict:
+            module = self.__feature_layer_dict[name]
+            x = module(x)
+            if name == self._target_layer:
+                if self.verbose:
+                    print(f"Target layer [{name}] found")
+                x.register_hook(self.__save_gradient)
+                conv_output = x  # Save the convolution output on that layer
+        return conv_output, x
+
+    def _forward_pass(self, surf, params):
+        """
+        Does a full forward pass on the model
+        """
+        # Forward pass on the convolutions
+        conv_output, x = self.__forward_pass_on_convolutions(surf)
+        x = self.__pnet_feature_to_classifier_opt(x)
+        x = self.model.output(x, params)
+
+        return conv_output, x
+
+    def __backward(self):
+
+        #! 重要提示：没有 requires_grad_() 就不会计算梯度
+        self._batch = [item.to(self.device).requires_grad_() for item in self._batch]
+        targets = self._batch[-1]
+        # 前向传播：计算模型的输出
+        conv_outputs, model_outputs = self._forward_pass(*self._batch[0:-1])
+        # 将梯度重置为零，为下一次反向传播做准备
+        self.model.zero_grad()
+        model_outputs.backward(gradient=targets, retain_graph=True)
+        self._guided_gradients = self._gradients.data.cpu().numpy()[0]
+        self._conv_outputs = conv_outputs.data.cpu().numpy()[0]
+
+    def __cam_to_fig(self):
+        self._cam = np.uint8(
+            (self._cam - np.min(self._cam))
+            / (np.max(self._cam) - np.min(self._cam))
+            * 255
+        )
+        self._cam = (
+            np.uint8(
+                Image.fromarray(self._cam).resize(
+                    (self._batch[0].shape[2], self._batch[0].shape[3]), Image.LANCZOS
+                )
+            )
+            / 255
+        )
+
+    @abstractmethod
+    def _cam_gen(self):
+        pass
+
+    def generate_cam(self, batch, target_layer: Optional[str]):
+        self._batch = batch
+        self.__set_target_layer(target_layer)
+        self.__backward()
+        self._cam_gen()
+        self.__cam_to_fig()
+        return self._cam, self._conv_outputs
+
+    def __forward(self):
+        def __densenet(x: torch.Tensor) -> torch.Tensor:
+            x = F.relu(x, inplace=True)
+            x = F.adaptive_avg_pool2d(x, (1, 1))
+            x = torch.flatten(x, 1)
+            x = self.__pretrained_net.classifier(x)
+            return x
+
+        def __resnet(x: torch.Tensor) -> torch.Tensor:
+            x = torch.flatten(x, 1)
+            x = self.__pretrained_net.fc(x)
+            return x
+
+        if isinstance(self.__pretrained_net, models.densenet.DenseNet):
+            return __densenet
+        if isinstance(self.__pretrained_net, models.resnet.ResNet):
+            return __resnet
+
+    def __get_feature_dict(self):
+        if isinstance(self.__pretrained_net, models.densenet.DenseNet):
+            self.__feature_layer_dict = {
+                name: module
+                for name, module in self.__pretrained_net.features.named_children()
+            }
+        elif isinstance(self.__pretrained_net, models.resnet.ResNet):
+            self.__feature_layer_dict = {
+                name: module
+                for name, module in self.__pretrained_net.named_children()
+                if name not in ["fc"]
+            }
+
+
+class GradCam(CamBase):
+    def _cam_gen(self):
+        self._weights = np.mean(self._guided_gradients, axis=(1, 2))
+        self._cam = np.ones(self._conv_outputs.shape[1:], dtype=np.float32)
+        for i, w in enumerate(self._weights):
+            self._cam += w * self._conv_outputs[i, :, :]
+
+        self._cam = np.maximum(self._cam, 0)
+
+
+class ScoreCam(CamBase):
+    def _cam_gen(self):
+        self._cam = np.ones(self._conv_outputs.shape[1:], dtype=np.float32)
+        self._conv_outputs = torch.tensor(
+            self._conv_outputs, dtype=torch.float32, device=self.device
+        )
+        self._weights = torch.zeros(
+            self._conv_outputs.shape[0], dtype=torch.float32, device=self.device
+        )
+        loss_func = nn.MSELoss()
+        for i in range(len(self._conv_outputs)):
+            saliency_map = torch.unsqueeze(
+                torch.unsqueeze(self._conv_outputs[i, :, :], 0), 0
+            )
+            saliency_map = F.interpolate(
+                saliency_map,
+                size=(self._batch[0].shape[2], self._batch[0].shape[3]),
+                mode="bilinear",
+                align_corners=False,
+            )
+            if saliency_map.max() == saliency_map.min():
+                continue
+            norm_saliency_map = (saliency_map - saliency_map.min()) / (
+                saliency_map.max() - saliency_map.min()
+            )
+            self._batch[0] = self._batch[0] * norm_saliency_map
+            _, outputs = self._forward_pass(*self._batch[0:-1])
+            #! 评分机制, 需要改进
+            # 1. 计算MSELoss
+            # 2. 将所有的1 / MSELoss进行softmax归一化
+            self._weights[i] = 1.0 / loss_func(outputs, self._batch[-1])
+        self._weights = F.softmax(self._weights, dim=0)
+        self._weights = self._weights.detach().cpu().numpy()
+        self._conv_outputs = self._conv_outputs.detach().cpu().numpy()
+        for i, w in enumerate(self._weights):
+            self._cam += w * self._conv_outputs[i, :, :]
+        self._cam = np.maximum(self._cam, 0)
+
+
+class LayerCam(CamBase):
+    def _cam_gen(self):
+        self._weights = self._guided_gradients
+        self._weights[self._weights < 0] = 0
+        self._cam = np.sum(self._weights * self._conv_outputs, axis=0)
